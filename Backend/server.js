f8dbbfb5a4b7b490021ee40f0ef378fdc95d1ecf
itamitbhai <yadavamit57354@gmail.com>
2026-04-19 17:20:51 +0530

    Ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -7,10 +7,12 @@ import morgan from "morgan";
 import debugMasterRoute from "./routes/debugMasterRoute.js";
 import codeRelayRoute from "./routes/codeRelayRoute.js";
 import hackathonRoute from "./routes/hackathonRoute.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const _dirname = path.resolve();
 
 //------------------//
 // Security         //
@@ -27,29 +29,35 @@ app.use(express.urlencoded({ extended: true }));
 //------------------//
 // ENV Based CORS   //
 //------------------//
-const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://apnaDomain.com"] : [process.env.FRONTEND_URL];
+// const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://apnaDomain.com"] : [process.env.FRONTEND_URL ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true); 
+
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("CORS not allowed ❌"));
+//     }
+//   },
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); 
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed ❌"));
-    }
-  },
+  origin: ("http://localhost:5173"),
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 //------------------//
 // Test route       //
 //------------------//
-app.get("/", (_ , res) => {
-  res.send("API is running...");
-});
+// app.get("/", (_ , res) => {
+//   res.send("API is running...");
+// });
 
 //------------------//
 // Routes           //
@@ -57,6 +65,13 @@ app.get("/", (_ , res) => {
 app.use("/event/debug-master", debugMasterRoute);
 app.use("/event/code-relay", codeRelayRoute);
 app.use("/event/hackathon", hackathonRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
+
 
 //------------------//
 // Start Server     //
