2ec427dd473e6f28e34c7c8fc8fb62c7a6545fd9
itamitbhai <yadavamit57354@gmail.com>
2026-04-20 21:39:39 +0530

    Tilt

--- Frontend/src/components/Tiltcard.jsx
+++ Frontend/src/components/Tiltcard.jsx
@@ -1,3 +1,50 @@
+// import { useRef } from "react";
+
+// const TiltCard = ({ children, className }) => {
+//   const cardRef = useRef(null);
+
+//   const handleMouseMove = (e) => {
+//     const card = cardRef.current;
+//     const rect = card.getBoundingClientRect();
+
+//     const x = e.clientX - rect.left;
+//     const y = e.clientY - rect.top;
+
+//     const rotateX = -(y - rect.height / 2) / 18;
+//     const rotateY = (x - rect.width / 2) / 18;
+
+//     card.style.transform = `
+//       perspective(1000px)
+//       rotateX(${rotateX}deg)
+//       rotateY(${rotateY}deg)
+//       scale(1.02)
+//     `;
+//   };
+
+//   //tilt card
+//   const resetTilt = () => {
+//     cardRef.current.style.transform =
+//       "perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1)";
+//   };
+
+//   return (
+//     <div
+//       ref={cardRef}
+//       onMouseMove={handleMouseMove}
+//       onMouseLeave={resetTilt}
+//       className={`transition-all duration-500 ${className}`}
+//       style={{
+//         transformStyle: "preserve-3d",
+//         transition: "transform 0.3s cubic-bezier(.03,.98,.52,.99)",
+//       }}
+//     >
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default TiltCard;
+
 import { useRef } from "react";
 
 const TiltCard = ({ children, className }) => {
